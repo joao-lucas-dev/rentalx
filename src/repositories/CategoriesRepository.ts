ac2cbb1e394799e80135a1691652aaf0a029dfa6
Joao Lucas <jlmelosouza18@gmail.com>
2021-03-28 13:25:55 -0300

    categories post and get

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,38 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategory {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ description, name }: ICreateCategory): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((item) => item.name === name);
+
+    return category;
+  }
+}
+
+export { CategoriesRepository };
