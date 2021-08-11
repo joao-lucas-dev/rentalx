d5a52a8249a77877a566ac10df64e7f32d468b34
João Lucas <jlmelosouza18@gmail.com>
2021-08-11 18:43:52 -0300

    specifications migration

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,17 +1,23 @@
-import { v4 as uuidV4 } from "uuid";
+import { Entity, Column, PrimaryColumn, CreateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
 
+@Entity("specifications")
 class Specification {
+  @PrimaryColumn()
   id?: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
     if (!this.id) {
-      this.id = uuidV4();
+      this.id = uuid();
     }
   }
 }
